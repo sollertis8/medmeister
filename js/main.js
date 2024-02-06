464f851d7d281257b7b043b8440879001d59c582
sollertis8 <mizumedia@gmail.com>
2024-02-06 12:14:27 -0500

    updated cors headers for getDoctor function

--- js/main.js
+++ js/main.js
@@ -183,6 +183,8 @@ function getDoctor(uid, callback) {
     CORS: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
     },
     success: callback,
   };
